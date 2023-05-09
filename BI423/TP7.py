a28a098c8716af5dabb79bf3af32f028f80e0cfe
Benjamin PREHAUD <benjamin.prehaud@ens.uvsq.fr>
2023-05-09 15:07:15 +0200

    save 09/05/2023

--- BI423/TP7.py
+++ BI423/TP7.py
@@ -1,37 +1,51 @@
-import mathplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
-fichier_1 = open(input("entrer nom fichier pour axe x"),"r")
-fichier_2 = open(input("entrer nom fichier pour axe y"),"r")
-tailles_fenetre = int(input("entrer la taille de la fenetre"))
+def dotplot(seq1,seq2,fenetre,identité):
+    fichier_1 = open(seq1,"r")
+    fichier_2 = open(seq2,"r")
+    tailles_fenetre = int(fenetre)
 
-ligne_x = fichier_1.readline()
-stockage_x = ""
-while ligne_x !="":
-    stockage_x += ligne_x[:len(ligne_x)-1]
     ligne_x = fichier_1.readline()
+    stockage_x = ""
+    while ligne_x !="":
+        stockage_x += ligne_x[:len(ligne_x)-1]
+        ligne_x = fichier_1.readline()
 
-ligne_y = fichier_2.readline()
-stockage_y = ""
-while ligne_y !="":
-    stockage_y += ligne_y[:len(ligne_y)-1]
     ligne_y = fichier_2.readline()
+    stockage_y = ""
+    while ligne_y !="":
+        stockage_y += ligne_y[:len(ligne_y)-1]
+        ligne_y = fichier_2.readline()
 
-fichier_1.close(),fichier_2.close()
+    fichier_1.close(),fichier_2.close()
 
 
-plt.title("Dot Plot") # titre du graphique
-plt.xlabel("Axe X") # label/nom de l'axe des abscisses
-plt.ylabel("Axe Y") # label/nom de l'axe des des ordonnees
-plt.xlim(0, len(stockage_x)) # taille/longueur de l'axe des "x"
-plt.ylim(0, len(stockage_y)) # taille/longueur de l'axe des "y"
-point_x = 0
-point_y = 0
-while point_x < len(stockage_x)-1:
-    while point_y < len(stockage_y)-1:
-        if stockage_x[point_x:(point_x+tailles_fenetre)-1] == stockage_y[point_y:(point_y+tailles_fenetre)-1]:
-            plt.scatter(point_x,point_y, c="red", s=15)
-        point_y += 1
-    point_y = 0
-    point_x += 1
-plt.savefig("TD7.png")
-plt.show()   
+    plt.title("Dot Plot") # titre du graphique
+    plt.xlabel("Axe X") # label/nom de l'axe des abscisses
+    plt.ylabel("Axe Y") # label/nom de l'axe des des ordonnees
+    plt.xlim(0, len(stockage_x)) # taille/longueur de l'axe des "x"
+    plt.ylim(0, len(stockage_y)) # taille/longueur de l'axe des "y"
+    
+    for x in range(0,len(stockage_x)-1):
+        for y in range(0,len(stockage_y)-1):
+            print((y+tailles_fenetre)-1)
+            if stockage_x[x:(x+tailles_fenetre)-1]== stockage_y[y:(y+tailles_fenetre)-1]:
+                plt.scatter(x,y, c="red", s=15)
+    
+    plt.savefig("TD7.png")
+    plt.show()
+
+
+
+def pidentité(x,y):
+    id= 0
+    for i in range(0,len(x)-1):
+        if x[i]==y[i]:
+            id+=1
+    pourcentage_id = (id // len(x))* 100
+    return pourcentage_id
+
+dotplot("seqx.txt","seqy.txt",2,0)
+
+# and pidentité(stockage_x[point_x:(point_x+tailles_fenetre)-1],stockage_y[point_y:(point_y+tailles_fenetre)-1])> identité
+
---
+++ IN405/TD8/exercice1v2.py
@@ -0,0 +1,5 @@
+import thread
+
+
+
+if __name__ == "__main__":
